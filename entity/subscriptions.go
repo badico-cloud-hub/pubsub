7514f5839dee860652a84f4d379efc19c94319ba
wellington gadelha <contato.informeai@gmail.com>
2022-12-20 09:47:20 -0300

    feat(entity/subscriptions.go): create entity to subscriptions

---
+++ entity/subscriptions.go
@@ -0,0 +1,15 @@
+package entity
+
+//Subscription is entity the subscription
+type Subscription struct {
+	PK                string `dynamodbav:"PK,omitempty"`
+	SK                string `dynamodbav:"SK,omitempty"`
+	GSIPK             string `dynamodbav:"GSIPK,omitempty"`
+	GSISK             string `dynamodbav:"GSISK,omitempty"`
+	ClientId          string `dynamodbav:"client_id,omitempty"`
+	SubscriptionEvent string `dynamodbav:"subscription_event,omitempty"`
+	SubscriptionUrl   string `dynamodbav:"subscription_url,omitempty"`
+	SubscriptionId    string `dynamodbav:"subscription_id,omitempty"`
+	CreatedAt         string `dynamodbav:"createdAt,omitempty"`
+	UpdatedAt         string `dynamodbav:"updatedAt,omitempty"`
+}
