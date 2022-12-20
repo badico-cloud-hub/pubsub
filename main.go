2501c1ca82102adcc7b90e195fe04eb563d375a7
wellington gadelha <contato.informeai@gmail.com>
2022-12-20 09:55:45 -0300

    chore(main.go): update logic of execute server api

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/badico-cloud-hub/pubsub/api"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	port := os.Getenv("PORT")
+	api := api.NewServer(port)
+	api.AllRouters()
+	if err := api.Run(); err != nil {
+		log.Fatal(err)
+	}
 
 }
