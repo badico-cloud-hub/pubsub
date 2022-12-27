aca14caf7758876cf6fc2ded41719aa5f4a00a5b
wellington gadelha <contato.informeai@gmail.com>
2022-12-27 11:41:08 -0300

    chore(api/server.go): add more routers

--- api/server.go
+++ api/server.go
@@ -18,6 +18,7 @@ import (
 type Server struct {
 	port           string
 	mux            *mux.Router
+	routersAdmin   *mux.Router
 	routersAuth    *mux.Router
 	routersNotAuth *mux.Router
 	logger         interfaces.ServiceLogger
@@ -36,12 +37,15 @@ func NewServer(port string) *Server {
 		log.Fatal(err)
 	}
 	router := mux.NewRouter()
+	adminRouters := router.PathPrefix("/").Subrouter()
 	authRouters := router.PathPrefix("/").Subrouter()
 	notAuthRouters := router.PathPrefix("/").Subrouter()
+	adminRouters.Use(utils.LoggingMiddleware, utils.SetupHeadersMiddleware)
 	authRouters.Use(utils.LoggingMiddleware, utils.SetupHeadersMiddleware, utils.AuthorizeMiddleware)
 	notAuthRouters.Use(utils.LoggingMiddleware, utils.SetupHeadersMiddleware)
 	return &Server{
 		mux:            router,
+		routersAdmin:   adminRouters,
 		routersAuth:    authRouters,
 		routersNotAuth: notAuthRouters,
 		logger:         utils.NewLogger(os.Stdout),
@@ -92,6 +96,56 @@ func (s *Server) TestNotification() {
 	s.routersAuth.HandleFunc("/subscriptions/test", s.testNotification).Methods(http.MethodPost)
 }
 
+//CreateServices execute creation of services
+func (s *Server) CreateServices() {
+	s.routersAdmin.HandleFunc("/services", s.createServices).Methods(http.MethodPost)
+}
+
+//GetServices return service events
+func (s *Server) GetServices() {
+	s.routersAdmin.HandleFunc("/services/{service}", s.getServices).Methods(http.MethodGet)
+}
+
+//GetServicesEvents return all events from service
+func (s *Server) GetServicesEvents() {
+	s.routersAdmin.HandleFunc("/services/{service}/events", s.getServicesEvents).Methods(http.MethodGet)
+}
+
+//ListServices return all services the table
+func (s *Server) ListServices() {
+	s.routersAdmin.HandleFunc("/services", s.listServices).Methods(http.MethodGet)
+}
+
+//DeletServices remove service or single events the service
+func (s *Server) DeleteServices() {
+	s.routersAdmin.HandleFunc("/services/{service}", s.deleteServices).Methods(http.MethodDelete)
+}
+
+//AddEventToService execute append the new events to service
+func (s *Server) AddEventToService() {
+	s.routersAdmin.HandleFunc("/services/{service}/events", s.addEvent).Methods(http.MethodPost)
+}
+
+//CreateClients execute creation the clients in table
+func (s *Server) CreateClients() {
+	s.routersAdmin.HandleFunc("/clients", s.createClients).Methods(http.MethodPost)
+}
+
+//GetClients return all clients by service
+func (s *Server) GetClients() {
+	s.routersAdmin.HandleFunc("/clients/{service}", s.getClients).Methods(http.MethodGet)
+}
+
+//ListClients return all clients the table
+func (s *Server) ListClients() {
+	s.routersAdmin.HandleFunc("/clients", s.listClients).Methods(http.MethodGet)
+}
+
+//DeleteClients remove client the service
+func (s *Server) DeleteClients() {
+	s.routersAdmin.HandleFunc("/clients/{service}/{identifier}", s.deleteClients).Methods(http.MethodDelete)
+}
+
 //AllRouters execute all routers the server
 func (s *Server) AllRouters() {
 	s.routersNotAuth.HandleFunc("/health", s.health).Methods(http.MethodGet)
@@ -99,6 +153,17 @@ func (s *Server) AllRouters() {
 	s.routersAuth.HandleFunc("/subscriptions", s.createSubscription).Methods(http.MethodPost)
 	s.routersAuth.HandleFunc("/subscriptions/{id}", s.deleteSubscription).Methods(http.MethodDelete)
 	s.routersAuth.HandleFunc("/subscriptions/test", s.testNotification).Methods(http.MethodPost)
+	s.routersAdmin.HandleFunc("/services", s.listServices).Methods(http.MethodGet)
+	s.routersAdmin.HandleFunc("/services", s.createServices).Methods(http.MethodPost)
+	s.routersAdmin.HandleFunc("/services/{service}", s.getServices).Methods(http.MethodGet)
+	s.routersAdmin.HandleFunc("/services/{service}/events", s.getServicesEvents).Methods(http.MethodGet)
+	s.routersAdmin.HandleFunc("/services/{service}", s.deleteServices).Methods(http.MethodDelete)
+	s.routersAdmin.HandleFunc("/services/{service}/events", s.addEvent).Methods(http.MethodPost)
+	s.routersAdmin.HandleFunc("/clients", s.createClients).Methods(http.MethodPost)
+	s.routersAdmin.HandleFunc("/clients", s.listClients).Methods(http.MethodGet)
+	s.routersAdmin.HandleFunc("/clients/{service}", s.getClients).Methods(http.MethodGet)
+	s.routersAdmin.HandleFunc("/clients/{service}/{identifier}", s.deleteClients).Methods(http.MethodDelete)
+
 }
 
 func (s *Server) health(w http.ResponseWriter, r *http.Request) {
@@ -187,6 +252,338 @@ func (s *Server) deleteSubscription(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (s *Server) createServices(w http.ResponseWriter, r *http.Request) {
+	serviceDto := dto.ServicesDTO{}
+	defer r.Body.Close()
+	if err := json.NewDecoder(r.Body).Decode(&serviceDto); err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	existService, _, err := s.Dynamo.ExistService(serviceDto.Name)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if existService {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceAlreadyExist.Error()})
+		return
+	}
+	//TODO: Verify if array events is empty, and return error
+	serviceId, err := s.Dynamo.CreateServices(serviceDto)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(dto.ServicesDTO{ServiceId: serviceId}); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+
+}
+func (s *Server) getServices(w http.ResponseWriter, r *http.Request) {
+	service := mux.Vars(r)["service"]
+	serviceEvents, err := s.Dynamo.GetServices(service)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	serviceDto := dto.ServicesDTO{}
+	if len(serviceEvents) > 0 {
+		serviceDto.Name = serviceEvents[0].Name
+		serviceDto.ServiceId = serviceEvents[0].ServiceId
+		serviceDto.CreatedAt = serviceEvents[0].CreatedAt
+		for _, event := range serviceEvents {
+			serviceDto.Events = append(serviceDto.Events, event.ServiceEvent)
+		}
+		if err := json.NewEncoder(w).Encode(&serviceDto); err != nil {
+			s.logger.Error(err.Error())
+			return
+		}
+
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		if err := json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceNotFound.Error()}); err != nil {
+			s.logger.Error(err.Error())
+			return
+		}
+	}
+
+}
+
+func (s *Server) getServicesEvents(w http.ResponseWriter, r *http.Request) {
+	service := mux.Vars(r)["service"]
+	event := r.URL.Query().Get("name")
+	serviceEvent, err := s.Dynamo.GetServicesEvents(service, event)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	serviceEventDTO := dto.ServiceEventsDTO{
+		Name:      serviceEvent.ServiceEvent,
+		Service:   serviceEvent.Name,
+		ServiceId: serviceEvent.ServiceId,
+		CreatedAt: serviceEvent.CreatedAt,
+		UpdatedAt: serviceEvent.UpdatedAt,
+	}
+	if err := json.NewEncoder(w).Encode(&serviceEventDTO); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+
+}
+
+func (s *Server) listServices(w http.ResponseWriter, r *http.Request) {
+	services, err := s.Dynamo.ListServices()
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+
+	allServices := []dto.ServicesDTO{}
+	mapServices := make(map[string]dto.ServicesDTO)
+	for _, serv := range services {
+		serviceDto := dto.ServicesDTO{
+			Name:      serv.Name,
+			ServiceId: serv.ServiceId,
+			CreatedAt: serv.CreatedAt,
+		}
+		mapServices[serv.Name] = serviceDto
+	}
+	for _, val := range mapServices {
+		allServices = append(allServices, val)
+	}
+	if err := json.NewEncoder(w).Encode(&allServices); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+
+}
+func (s *Server) deleteServices(w http.ResponseWriter, r *http.Request) {
+	service := mux.Vars(r)["service"]
+	eventParam := r.URL.Query().Get("event")
+	servicesEvents, err := s.Dynamo.GetServices(service)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+
+	if eventParam != "" {
+		for _, ev := range servicesEvents {
+			if eventParam == ev.ServiceEvent {
+				if err := s.Dynamo.DeleteServices(ev.ServiceId, eventParam); err != nil {
+					s.logger.Error(err.Error())
+				}
+				break
+			}
+		}
+	} else {
+		for _, ev := range servicesEvents {
+			if err := s.Dynamo.DeleteServices(ev.ServiceId, ev.ServiceEvent); err != nil {
+				s.logger.Error(err.Error())
+				continue
+			}
+		}
+	}
+	if err := json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "success"}); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+}
+
+func (s *Server) addEvent(w http.ResponseWriter, r *http.Request) {
+	service := mux.Vars(r)["service"]
+	defer r.Body.Close()
+	payload := dto.ServicesDTO{}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+	exists, serviceId, err := s.Dynamo.ExistService(service)
+	if err != nil {
+		s.logger.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if exists {
+		//TODO: execute append with array of events the payload
+		existedEvent, err := s.Dynamo.GetServicesEvents(service, payload.Name)
+		if err != nil && err != infra.ErrorServiceEventNotFound {
+			s.logger.Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+			return
+		}
+		if existedEvent.ServiceEvent == payload.Name {
+			s.logger.Error(infra.ErrorServiceEventAlreadyExist.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceEventAlreadyExist.Error()})
+			return
+		}
+		_, err = s.Dynamo.PutEventService(service, serviceId, payload.Name)
+		if err != nil {
+			s.logger.Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		if err := json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "success"}); err != nil {
+			s.logger.Error(err.Error())
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		if err := json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceNotFound.Error()}); err != nil {
+			s.logger.Error(err.Error())
+			return
+		}
+
+	}
+
+}
+func (s *Server) createClients(w http.ResponseWriter, r *http.Request) {
+	client := dto.ClientDTO{}
+	defer r.Body.Close()
+	if err := json.NewDecoder(r.Body).Decode(&client); err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	_, exists, err := s.Dynamo.ExistClient(client.Identifier, client.Service)
+	if err != nil && err != infra.ErrorClientNotFound {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if exists {
+		s.logger.Error(infra.ErrorClientAlreadyExist.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorClientAlreadyExist.Error()})
+		return
+	}
+	existService, _, err := s.Dynamo.ExistService(client.Service)
+	if err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if !existService {
+		s.logger.Error(infra.ErrorServiceNotFound.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceNotFound.Error()})
+		return
+	}
+	apiKey, err := s.Dynamo.CreateClients(client)
+	if err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if err := json.NewEncoder(w).Encode(&dto.ClientDTO{ApiKey: apiKey}); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+}
+
+func (s *Server) getClients(w http.ResponseWriter, r *http.Request) {
+	service := mux.Vars(r)["service"]
+	identifier := r.URL.Query().Get("identifier")
+	defer r.Body.Close()
+
+	exist, _, err := s.Dynamo.ExistService(service)
+	if err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	if !exist {
+		s.logger.Error(infra.ErrorServiceNotFound.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: infra.ErrorServiceNotFound.Error()})
+		return
+	}
+	clients, err := s.Dynamo.ListClients()
+	if err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	responseClients := []dto.ClientDTO{}
+	if len(identifier) > 0 {
+		for _, client := range clients {
+			if client.Service == service && client.Identifier == identifier {
+				newClient := dto.ClientDTO{
+					ApiKey:     client.GSIPK,
+					Identifier: client.Identifier,
+					Service:    client.Service,
+					CreatedAt:  client.CreatedAt,
+					UpdatedAt:  client.UpdatedAt,
+				}
+				responseClients = append(responseClients, newClient)
+			}
+		}
+	} else {
+		for _, client := range clients {
+			if client.Service == service {
+				newClient := dto.ClientDTO{
+					ApiKey:     client.GSIPK,
+					Identifier: client.Identifier,
+					Service:    client.Service,
+					CreatedAt:  client.CreatedAt,
+					UpdatedAt:  client.UpdatedAt,
+				}
+				responseClients = append(responseClients, newClient)
+			}
+		}
+
+	}
+	if err := json.NewEncoder(w).Encode(&responseClients); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+}
+func (s *Server) listClients(w http.ResponseWriter, r *http.Request) {
+	clients, err := s.Dynamo.ListClients()
+	if err != nil {
+		s.logger.Error(err.Error())
+		_ = json.NewEncoder(w).Encode(&dto.ResponseDTO{Status: "error", Message: err.Error()})
+		return
+	}
+	responseClients := []dto.ClientDTO{}
+	for _, client := range clients {
+		respClient := dto.ClientDTO{
+			ApiKey:     client.GSIPK,
+			Identifier: client.Identifier,
+			Service:    client.Service,
+			CreatedAt:  client.CreatedAt,
+			UpdatedAt:  client.UpdatedAt,
+		}
+		responseClients = append(responseClients, respClient)
+	}
+
+	if err := json.NewEncoder(w).Encode(&responseClients); err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+}
+func (s *Server) deleteClients(w http.ResponseWriter, r *http.Request) {
+	//TODO: delete clients
+}
+
 func (s *Server) testNotification(w http.ResponseWriter, r *http.Request) {
 	notif := dto.NotifierDTO{
 		ClientId: r.Header.Get("client-id"),
@@ -202,7 +599,8 @@ func (s *Server) testNotification(w http.ResponseWriter, r *http.Request) {
 		SentTo: []dto.SubscriptionDTO{},
 	}
 	subscription, err := s.Dynamo.GetSubscription(notif.ClientId, notif.Event)
-	if err != nil && err.Error() == "not subscription event in table" {
+	if err != nil && err == infra.ErrorSubscriptinEventNotFound {
+		w.WriteHeader(http.StatusNotFound)
 		if err := json.NewEncoder(w).Encode(&respNotSubscriptions); err != nil {
 			s.logger.Error(err.Error())
 			return
@@ -246,6 +644,7 @@ func (s *Server) testNotification(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		if err := json.NewEncoder(w).Encode(&respNotSubscriptions); err != nil {
 			s.logger.Error(err.Error())
 			return
