c6c5929798afcaa3c92365d219bddd546c88312b
Edson Júnior <edsonjunior.1219@gmail.com>
2023-01-17 14:39:32 -0300

    refac(entity/services.go): change services properties

--- entity/services.go
+++ entity/services.go
@@ -2,14 +2,15 @@ package entity
 
 //Services is entity the services
 type Services struct {
-	PK           string `dynamodbav:"PK,omitempty"`
-	SK           string `dynamodbav:"SK,omitempty"`
-	GSIPK        string `dynamodbav:"GSIPK,omitempty"`
-	GSISK        string `dynamodbav:"GSISK,omitempty"`
-	ApiKey       string `dynamodbav:"api_key,omitempty"`
-	Name         string `dynamodbav:"name,omitempty"`
-	ServiceId    string `dynamodbav:"service_id,omitempty"`
-	ServiceEvent string `dynamodbav:"service_event,omitempty"`
-	CreatedAt    string `dynamodbav:"createdAt,omitempty"`
-	UpdatedAt    string `dynamodbav:"updatedAt,omitempty"`
+	PK                string `dynamodbav:"PK,omitempty"`
+	SK                string `dynamodbav:"SK,omitempty"`
+	INDEX_AUXILIAR_PK string `dynamodbav:"INDEX_AUXILIAR_PK,omitempty"`
+	INDEX_AUXILIAR_SK string `dynamodbav:"INDEX_AUXILIAR_SK,omitempty"`
+	ApiKey            string `dynamodbav:"api_key,omitempty"`
+	Name              string `dynamodbav:"name,omitempty"`
+	ServiceId         string `dynamodbav:"service_id,omitempty"`
+	ServiceEvent      string `dynamodbav:"service_event,omitempty"`
+	Entity            string `dynamodbav:"entity,omitempty"`
+	CreatedAt         string `dynamodbav:"createdAt,omitempty"`
+	UpdatedAt         string `dynamodbav:"updatedAt,omitempty"`
 }
