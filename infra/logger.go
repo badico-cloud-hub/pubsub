173a020a00546e8cfe0cdf21ff5596f4f6a60ae0
Edson Júnior <edsonjunior.1219@gmail.com>
2023-01-27 15:05:18 -0300

    feat(infra/logger.go): log manager

---
+++ infra/logger.go
@@ -0,0 +1,38 @@
+package infra
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/badico-cloud-hub/log-driver/logger"
+	"github.com/badico-cloud-hub/log-driver/producer"
+)
+
+func NewLogManager() *producer.LoggerManager {
+
+	logManager := producer.NewLoggerManager(logger.LogContext{
+		AppName:    "Pubsubb API",
+		AppType:    "API",
+		AppVersion: "BEST BE FROM GIT",
+		Machine:    os.Getenv("MACHINE_IP"),
+	})
+
+	err := logManager.Setup(
+		"us-east-2",
+		os.Getenv("LOG_ACCESS_KEY_ID"),
+		os.Getenv("LOG_ACCESS_KEY"),
+		os.Getenv("LOG_MESSAGE_QUEUE_URL"),
+		os.Getenv("LOG_EVENT_QUEUE_URL"),
+	)
+
+	if err != nil {
+		fmt.Println("============================================")
+		fmt.Println("WARN: CLOUDLOG ENGINE NOT WORKING")
+		fmt.Println("============================================")
+		fmt.Println(err)
+		return nil
+	}
+
+	return logManager
+
+}
