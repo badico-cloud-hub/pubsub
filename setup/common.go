9290959e4f99c59b29f7f211bed4a789b41be20d
Edson Júnior <edsonjunior.1219@gmail.com>
2023-02-15 17:36:09 -0300

    refac(setup/common.go): remove interval

--- setup/common.go
+++ setup/common.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/badico-cloud-hub/log-driver/producer"
@@ -15,14 +14,13 @@ import (
 func SetupNotifyEventConsumer(sqs *sqs.SQS, wg *sync.WaitGroup, logManager *producer.LoggerManager) {
 	queueUrl := os.Getenv("NOTIFY_SUBSCRIBERS_QUEUE_URL")
 	dlq := os.Getenv("NOTIFY_SUBSCRIBERS_QUEUE_URL_DLQ")
-	interval := 100 * time.Millisecond
 
 	setupLog := logManager.NewLogger("logger setup notify event consumer - ", os.Getenv("MACHINE_IP"))
 	dynamoClient := infra.NewDynamodbClient()
 	if err := dynamoClient.Setup(); err != nil {
 		setupLog.Errorln(err.Error())
 	}
-	consumer, err := consumer.NewSQSConsumer(queueUrl, dlq, sqs, consumer.NewNotifyEventHandler(logManager), 10, interval, logManager, dynamoClient)
+	consumer, err := consumer.NewSQSConsumer(queueUrl, dlq, sqs, consumer.NewNotifyEventHandler(logManager), 10, logManager, dynamoClient)
 
 	if err != nil {
 		fmt.Println(err)
